e4d4ddb91b4bd2a1aceca8a26e014f07965e7ec8
Enes Veliu <enesveliu2015@gmail.com>
2026-06-25 21:22:21 +0200

    add temperature conversion utiliy

--- basics/index.js
+++ basics/index.js
@@ -19,6 +19,17 @@ function classifyIP(ip) {
 console.log(classifyIP("192.168.1.1"));
 console.log(classifyIP("8.8.8.8"));
 
+// Temperature Converter
+
+function celsiusToFahrenheit(celsius) {
+    const fahrenheit = (celsius * 9/5) + 32;
+
+    return `${celsius}C = ${fahrenheit}F`;
+}
+
+console.log(celsiusToFahrenheit(30));
+
+
 // Bandwidth Calculator
 
 function mbpsToMBps(mbps) {
