47c4efc54f22f9bd0a8b8d4cf55d5515780825d3
Enes Veliu <enesveliu2015@gmail.com>
2026-06-25 21:20:29 +0200

    add IP classification utility

--- basics/index.js
+++ basics/index.js
@@ -6,3 +6,15 @@ let clientCount = 1500;
 console.log(companyName);
 console.log(clientCount);
 
+// IP Address Classifier
+
+function classifyIP(ip) {
+    if (ip.startsWith("192.168")) {
+        return "Private Network";
+    }
+
+    return "Public Network";
+}
+
+console.log(classifyIP("192.168.1.1"));
+console.log(classifyIP("8.8.8.8"));
